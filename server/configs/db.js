950b3960f9243cfe1f0c21d262e1eecb184fac53
balaraju0616 <balaraju1106@gmail.com>
2025-11-01 20:25:15 +0530

    backend

--- server/configs/db.js
+++ server/configs/db.js
@@ -3,9 +3,9 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
     try {
         mongoose.connection.on('connected', () => console.log('Database connected'))
-        await mongoose.connect(`${process.env.MONGODB_URL}/vibes`)
+        await mongoose.connect(process.env.MONGO_URI) // Use MONGO_URI directly
     } catch (error) {
-        console.log(error.message)
+        console.log('MongoDB connection error:', error.message)
     }
 }
 
